44b9143db24639c1e6c182ebfb46ac870514768e
Flaviu Ratiu <ratiu.flaviu@gmail.com>
2018-04-26 19:20:08 +0300

    added json report

--- src/test/java/org/fasttrackit/runners/ProductsSortingRunner.java
+++ src/test/java/org/fasttrackit/runners/ProductsSortingRunner.java
@@ -5,7 +5,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @CucumberOptions(
-        plugin = {"html:target/cucumber"},
+        plugin = {"html:target/cucumber", "json:target/json-reports/productsSorting.json"},
         glue = {"org.fasttrackit"},
         features = {"src/test/resources/features/products-sorting.feature"}
 )
